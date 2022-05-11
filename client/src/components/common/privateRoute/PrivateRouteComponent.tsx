9d6e958e050af387b0fdc235a48157c3e90cba5d
Brycen Medart <Brycemedart@gmail.com>
2022-05-11 14:23:43 -0700

    Fix - Private route had invalid usestate - Fixed issue where private route was defaulting to isAuth=false due to changes required by typescript. Added loading usestate to wait for useeffect call.

--- client/src/components/common/privateRoute/PrivateRouteComponent.tsx
+++ client/src/components/common/privateRoute/PrivateRouteComponent.tsx
@@ -11,35 +11,42 @@ const PrivateRouteComponent = ({
   children: ReactNode;
 }): ReactElement => {
   const [isAuth, setIsAuth] = useState(Boolean);
+  const [load, setLoad] = useState(true);
 
   //Verify JWT token on page load and set useState based on response
   useEffect(() => {
     const localAuth = VerifyLocalAuth();
-
     const isSessionValid = async () => {
       //Check for local token first before api call
       if (localAuth.status === 'valid') {
         const serverAuth = await VerifyJWTUtil();
 
         if (serverAuth.status === 'success') {
+          setLoad(false);
           setIsAuth(true);
         } else {
+          setLoad(false);
           setIsAuth(false);
         }
       } else {
+        setLoad(false);
         setIsAuth(false);
       }
     };
     isSessionValid();
   }, []);
 
-  if (isAuth === true) {
-    return <section className='dashboard'>{children}</section>;
-  } else if (isAuth === false) {
-    return <Navigate to='/login' />;
-  } else {
+  if (load === true) {
     return <SpinnerComponent type='full' size='60px' />;
+  } else {
+    if (isAuth === true) {
+      return <section className='dashboard'>{children}</section>;
+    } else if (isAuth === false) {
+      return <Navigate to='/login' />;
+    }
   }
+  //default return for typescript
+  return <SpinnerComponent type='full' size='60px' />;
 };
 
 export default PrivateRouteComponent;
